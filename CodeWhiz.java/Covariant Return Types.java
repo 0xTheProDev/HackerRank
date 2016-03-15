2dc6b88eea3c1b1f0ec17fa5afc03e98515aaa90
Progyan Bhattacharya <bprogyan@gmail.com>
2016-03-15 12:33:30 +0530

    Create Covariant Return Types.java

---
+++ CodeWhiz.java/Covariant Return Types.java
@@ -0,0 +1,47 @@
+class Flower{
+    String whats_Your_Name(){
+        return "I have many names and types.";
+    }
+}
+class Jasmine extends Flower{
+    @Override
+    String whats_Your_Name(){
+        return "Jasmine";
+    }
+}
+class Lily extends Flower{
+    @Override
+    String whats_Your_Name(){
+        return "Lily";
+    }
+}
+class Lotus extends Flower{
+    @Override
+    String whats_Your_Name(){
+        return "Lotus";
+    }
+}
+
+class State{
+    Flower your_National_Flower(){
+        return new Flower();
+    }
+}
+class WestBengal extends State{
+    @Override
+    Jasmine your_National_Flower(){
+        return new Jasmine();
+    }
+}
+class Karnataka extends State{
+    @Override
+    Lotus your_National_Flower(){
+        return new Lotus();
+    }
+}
+class AndhraPradesh extends State{
+    @Override
+    Lily your_National_Flower(){
+        return new Lily();
+    }
+}
